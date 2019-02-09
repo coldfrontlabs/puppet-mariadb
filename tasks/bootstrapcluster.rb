781517396740718196e16f2aa6e8932fc93c204b
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2019-02-09 08:56:09 -0500

    feat(task) add cluster bootstrap task

--- tasks/bootstrapcluster.rb
+++ tasks/bootstrapcluster.rb
@@ -1,1 +1,31 @@
-#! /opt/puppetlabs/puppet/bin/ruby
+#!/opt/puppetlabs/puppet/bin/ruby
+
+require 'json'
+require 'open3'
+require 'puppet'
+
+def bootstrapcluster()
+  if service_name.nil?
+    stdout, _stderr, _status = Open3.capture3('facter', '-p', 'os.family')
+    osfamily = stdout.strip
+    stdout, _stderr, _status = Open3.capture3('facter', '-p', 'os.release.version')
+    osversion = stdout.strip
+    cmd_string =  if osfamily == 'RedHat' and osversion == '6'
+                     'service mysql bootstrap'
+                  else
+                     'galera_new_cluster'
+                  end
+  end
+  _stdout, stderr, status = Open3.capture3(cmd_string)
+  raise Puppet::Error, stderr if status != 0
+  { status: "Cluster bootstrap successful" }
+end
+
+begin
+  result = bootstrapcluster()
+  puts result.to_json
+  exit 0
+rescue Puppet::Error => e
+  puts({ status: 'failure', error: e.message }.to_json)
+  exit 1
+end
